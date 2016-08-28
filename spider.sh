af28003c123cbb4b870598b25bec59caedf15ff2
Tom Fenech <tomjwfenech@gmail.com>
2016-08-28 23:49:22 +0200

    Use loop counter instead of separate variable
    Use bash built-in brace expansion instead of calling seq.

--- spider.sh
+++ spider.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-a = 0    
-    for i in `seq 1 255`;
-        do
-            a = a + 1
-            echo $i
-            wget -r --spider -D --header="Accept: text/html"  --user-agent="Order Of The Mouse: Spider-Robot" 139.162.246.$a:80
-        done
+
+for i in {1..255}
+do
+    echo "$i"
+    wget -r --spider -D --header="Accept: text/html"  \
+         --user-agent="Order Of The Mouse: Spider-Robot" "139.162.246.$i:80"
+done
